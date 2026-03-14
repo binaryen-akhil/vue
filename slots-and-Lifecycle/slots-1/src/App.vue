03873b1f133e577d44ada7a3260a389a68401396
akhil <akhil.bindhu@outlook.com>
2026-03-14 17:17:22 +0530

    slot and lifecycle

---
+++ slots-and-Lifecycle/slots-1/src/App.vue
@@ -0,0 +1,22 @@
+
+
+<template>
+<button @click="changeVersion"> version 1</button>
+<button @click="changeVersion"> version 2</button>
+
+<component :is="luckynumber" />
+</template>
+
+<script setup>
+import LuckyNumberV1 from './components/LuckyNumberV1.vue';
+import LuckyNumberV2 from './components/LuckyNumberV2.vue';
+import { ref , computed} from 'vue';
+const isLuckyNumberV1 = ref(true);
+
+const luckynumber = computed(() => (isLuckyNumberV1.value ? LuckyNumberV1 : LuckyNumberV2));
+function changeVersion() {
+    isLuckyNumberV1.value = !isLuckyNumberV1.value;
+}
+</script>
+
+<style scoped></style>
---
+++ slots-and-Lifecycle/slots-1/src/components/LuckyNumberV2.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="d-flex justify-content-center  vh-50">
+        <div  class=" mt-5 p-20 bg-primary w-50" >
+        <h2>Generate Lucky Number 2</h2>
+        <input type="number" v-model="maxNumber" class=" form-control mb-3 w-25  d-flex justify-content-center" placeholder="Enter max number" />
+        <button @click="generateLuckyNumber" class="btn btn-primary">Generate</button>
+        <p>Your lucky number is: {{ luckyNumber }}</p>
+        <p v-if="luckyNumber ==0">Generate Lucky Number Range 1 to {{ maxNumber }}</p>
+        <p v-else>click to generate lucky number</p>
+        
+    </div>
+    </div>
+</template>
+<script setup>
+import { ref } from 'vue';
+
+let luckyNumber = ref(0);
+function generateLuckyNumber() {
+    luckyNumber.value = Math.floor(Math.random() * maxNumber.value) + 1;
+}
+const maxNumber = ref(100);
+</script>
+<style scoped></style>
